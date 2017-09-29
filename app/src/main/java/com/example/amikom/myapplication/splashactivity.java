7e3b6de60e3bcbb7188ac0632c0a133281b3ec09
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2017-09-29 10:29:01 +0700

    first commit

---
+++ app/src/main/java/com/example/amikom/myapplication/splashactivity.java
@@ -0,0 +1,13 @@
+package com.example.amikom.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splashactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+    }
+}
